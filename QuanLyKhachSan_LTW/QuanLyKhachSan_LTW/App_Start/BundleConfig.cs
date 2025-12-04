f60c771694b8a4a07d08f446c41bdd821bf7ac43
Lâm Ngô <lamngo031@gmail.com>
2025-12-04 23:49:56 +0700

    Fix loi CSS giao dien trang chu

--- QuanLyKhachSan_LTW/QuanLyKhachSan_LTW/App_Start/BundleConfig.cs
+++ QuanLyKhachSan_LTW/QuanLyKhachSan_LTW/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace QuanLyKhachSan_LTW
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
